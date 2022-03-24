6bcf70ce531102e514a5036bdf673f3bfc26e973
ikehikeh151 <ikehikeh151@gmail.com>
2022-03-24 10:59:42 +0700

    change hotmail

--- src/helpers/sendOtp.js
+++ src/helpers/sendOtp.js
@@ -1,7 +1,7 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  service: "hotmail",
   auth: {
     user: process.env.EMAIL_SENDER,
     pass: process.env.PASSWORD_SENDER
